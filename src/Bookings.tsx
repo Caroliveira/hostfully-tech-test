13a3ebff0bdfe492e89b4c612acb04f07e5acec3
LinaOliveira <carolina.oliveira@andela.com>
2023-11-19 20:22:00 -0300

    Create basic design for the page

--- src/Bookings.tsx
+++ src/Bookings.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './Bookings.scss';
+import "./Bookings.scss";
 
 const Bookings = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="bookings">
+      <header className="bookings__header">
+        <h1>Hostfully Tech Test</h1>
       </header>
     </div>
   );
-}
+};
 
 export default Bookings;
